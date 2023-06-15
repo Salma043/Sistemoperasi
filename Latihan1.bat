1cc49915ba203319d55fbdb338a4f562d6df495a
Salma043 <136711973+Salma043@users.noreply.github.com>
2023-06-15 20:04:11 +0700

    Create Latihan1.bat
    Menambahkan file latihan 1.bat

---
+++ Latihan1.bat
@@ -0,0 +1,12 @@
+@echo off
+title IDENTITAS DIRI
+echo NIM                : 20122076
+echo Nama               : Salma Nabila
+echo No. WA             : 087788194457
+echo Alamat             : Ds. Wringingintung
+echo Status             : Belum Menikah
+echo Motto              : kalo orang lain bisa kenapa harus saya
+echo Pacar              : -
+echo Rencana Menikah    : 1 suami spek Jeno cukup
+echo Rencana Anak       : 2 aja
+pause
